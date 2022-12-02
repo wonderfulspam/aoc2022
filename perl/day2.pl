6a67a8b212bc514b778b83a1e1f04abe5debb9ef
Emil Thorenfeldt <emt@magenta-aps.dk>
2022-12-02 11:06:41 +0100

    Refactor

--- perl/day2.pl
+++ perl/day2.pl
@@ -19,15 +19,15 @@ sub part1 {
         my @choices = split ' ', $line;
         no warnings 'experimental';
         given (\@choices) {
-            $score += 4 when $_->[0] eq "A" && $_->[1] eq "X"; # Rock + Rock = Draw
-            $score += 8 when $_->[0] eq "A" && $_->[1] eq "Y"; # Rock + Paper = Win
-            $score += 3 when $_->[0] eq "A" && $_->[1] eq "Z"; # Rock + Scissors = Loss
-            $score += 1 when $_->[0] eq "B" && $_->[1] eq "X"; # Paper + Rock = Loss
-            $score += 5 when $_->[0] eq "B" && $_->[1] eq "Y"; # Paper + Paper = Draw
-            $score += 9 when $_->[0] eq "B" && $_->[1] eq "Z"; # Paper + Scissors = Win
-            $score += 7 when $_->[0] eq "C" && $_->[1] eq "X"; # Scissors + Rock = Win
-            $score += 2 when $_->[0] eq "C" && $_->[1] eq "Y"; # Scissors + Paper = Loss
-            $score += 6 when $_->[0] eq "C" && $_->[1] eq "Z"; # Scissors + Scissors = Draw
+            $score += 4 when $_->[1] eq "X" && $_->[0] eq "A"; # Rock + Rock = Draw
+            $score += 1 when $_->[1] eq "X" && $_->[0] eq "B"; # Rock + Paper = Loss
+            $score += 7 when $_->[1] eq "X" && $_->[0] eq "C"; # Rock + Scissors = Win
+            $score += 8 when $_->[1] eq "Y" && $_->[0] eq "A"; # Paper + Rock = Win
+            $score += 5 when $_->[1] eq "Y" && $_->[0] eq "B"; # Paper + Paper = Draw
+            $score += 2 when $_->[1] eq "Y" && $_->[0] eq "C"; # Paper + Scissors = Loss
+            $score += 3 when $_->[1] eq "Z" && $_->[0] eq "A"; # Scissors + Rock = Loss
+            $score += 9 when $_->[1] eq "Z" && $_->[0] eq "B"; # Scissors + Paper = Win
+            $score += 6 when $_->[1] eq "Z" && $_->[0] eq "C"; # Scissors + Scissors = Draw
         }
         use warnings;
     }
